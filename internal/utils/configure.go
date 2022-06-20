ad5dc232588ece1ff0c060ea9bb603a0100d93d3
Ayrat Burnashev <airatbyr@ya.ru>
2022-06-20 21:49:35 +0300

    feat: add config file

---
+++ internal/utils/configure.go
@@ -0,0 +1,39 @@
+package utils
+
+import (
+	"flag"
+	"os"
+)
+
+type Config struct {
+	RunAddress           string
+	DatabaseURI          string
+	AccrualSystemAddress string
+}
+
+func GetConfigs() *Config {
+	config := &Config{}
+
+	config.RunAddress = os.Getenv("RUN_ADDRESS")
+	if len(config.RunAddress) == 0 {
+		flag.StringVar(&config.RunAddress, "a", "localhost:8080", "server run address")
+	}
+	//config.BaseURL = os.Getenv("BASE_URL")
+	//if len(config.BaseURL) == 0 {
+	//	flag.StringVar(&config.BaseURL, "b", "http://localhost:8080", "base url")
+	//}
+
+	config.DatabaseURI = os.Getenv("DATABASE_URI")
+	if len(config.DatabaseURI) == 0 {
+		flag.StringVar(&config.DatabaseURI, "d", "", "db storage path")
+	}
+
+	//TODO - какой адрес? как подключать
+	config.AccrualSystemAddress = os.Getenv("ACCRUAL_SYSTEM_ADDRESS")
+	if len(config.DatabaseURI) == 0 {
+		flag.StringVar(&config.DatabaseURI, "r", "", "accrual system address")
+	}
+
+	flag.Parse()
+	return config
+}
